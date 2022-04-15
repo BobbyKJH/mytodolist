e3825cd430bcb7832330ca546514bcaf12405c1e
BobbyKJH <rlatnguscjsw@gmail.com>
2022-04-15 17:52:56 +0900

    feat: Home.jsx and Home.module.css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,18 @@ function Home() {
   return (
     <>
       <Nav />
-      <div className={styles.welcome}>1</div>
+      <div className={styles.welcome}>
+        <div className={styles.cover}>
+          <h1>Welcome TodoList</h1>
+          <br />
+          <br />
+          <h2>Hello Everyone!! </h2>
+          <p>
+            The site is the site that produces your todolist. I hope you will
+            make a list of your today week month to year to do and try it out.
+          </p>
+        </div>
+      </div>
     </>
   );
 }
